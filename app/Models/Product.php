cfbe773623aa73083134f9549dc22e658ef394b5
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-03-12 11:33:26 +0000

    Updated Models with fillable & relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Rayon;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+        'rayon_id',
+        'category',
+        'is_promoted',
+        'popularity',
+    ];
+
+    public function rayon(): BelongsTo
+    {
+        return $this->belongsTo(Rayon::class);
+    }
 }
--- app/Models/Rayon.php
+++ app/Models/Rayon.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Rayon extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
