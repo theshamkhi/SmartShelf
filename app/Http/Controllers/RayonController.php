6f0f15a5e31cd002ba1e85a61de0741712085f51
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-03-12 11:31:44 +0000

    Created necessary methods for RayonController

--- app/Http/Controllers/RayonController.php
+++ app/Http/Controllers/RayonController.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rayon;
 use Illuminate\Http\Request;
 
 class RayonController extends Controller
 {
-    //
+    public function index()
+    {
+        $rayons = Rayon::all();
+        return response()->json($rayons);
+    }
+
+    public function show($id)
+    {
+        $rayon = Rayon::find($id);
+        if (!$rayon) {
+            return response()->json(['message' => 'Ray not found'], 404);
+        }
+        return response()->json($rayon);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $rayon = Rayon::create($request->all());
+        return response()->json($rayon, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $rayon = Rayon::find($id);
+        if (!$rayon) {
+            return response()->json(['message' => 'Ray not found'], 404);
+        }
+
+        $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $rayon->update($request->all());
+        return response()->json($rayon);
+    }
+
+    public function destroy($id)
+    {
+        $rayon = Rayon::find($id);
+        if (!$rayon) {
+            return response()->json(['message' => 'Ray not found'], 404);
+        }
+
+        $rayon->delete();
+        return response()->json(['message' => 'Ray removed']);
+    }
 }
